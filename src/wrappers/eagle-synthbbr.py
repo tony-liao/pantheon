9d9f062fb151f44e10133fd8456f9b76ae3ee847
tony-liao <tony.liao.5966@gmail.com>
2020-04-04 00:30:11 -0400

    add module for synthesized bbr

---
+++ src/wrappers/eagle-synthbbr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from os import path
+from subprocess import check_call
+import subprocess
+
+import arg_parser
+import context
+
+def main():
+        args = arg_parser.sender_first()
+        cc_repo = path.join(context.third_party_dir, 'eagle-synthbbr')
+        send_src = path.join(cc_repo, 'sender-receiver/sender-receiver/sender_receiver/envs', 'example_xentropy.py')
+        recv_src = path.join(cc_repo, 'sender-receiver/sender-receiver/sender_receiver/envs', 'run_receiver.py')
+        if args.option == 'setup':
+            return
+
+        if args.option == 'sender':
+            cmd = ['python3', send_src, args.port, '--expert', 'SynthesizedBBR']
+            check_call(cmd)
+            return
+
+        if args.option == 'receiver':
+            cmd = ['python3', recv_src, args.ip, args.port]
+            check_call(cmd)
+            return
+
+if __name__ == "__main__":
+    main()
